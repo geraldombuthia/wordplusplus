26ceb72fecedd17d443833f79b92a0d3d60e9772
geraldombuthia <geraldombuthia@gmail.com>
2022-03-13 15:12:04 +0300

    Added search functionalities for single words and synonyms

--- routes/words.js
+++ routes/words.js
@@ -4,76 +4,75 @@ const Word = require("../models/word");
 
 //gettting all
 router.get("/", async (req, res) => {
-    try {
-        const words = await Word.find();
-        res.json(words);
-    } catch (err) {
-        res.status(500).json({ message: err.message })
-    }
-})
+  try {
+    const words = await Word.find();
+    res.json(words);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 //getting one
 router.get("/:id", getWord, (req, res) => {
-    res.json(res.word)
-})
+  res.json(res.word);
+});
 //getting with a query
 router.get("/single/:word", getQueryWord, (req, res) => {
-    res.json(res.word)
-})
+  res.json(res.word);
+});
 //creating one
 router.post("/", async (req, res) => {
-    const { word, word_type, plural, definition, example, synonyms } = req.body;
-    const wordobj = new Word({
-        word,
-        word_type,
-        plural,
-        definition,
-        example,
-        synonyms
-    })
-        Word.findOne({ word: word }, async (err, worddoc) => {
-            if (worddoc) {
-               return res.json({ message: "Word already exists" })
-            }
-            const newWord = await wordobj.save()
-            res.status(201).json(newWord)
-        })
-        
-    } catch (err) {
-        res.status(400).json({ message: err.message })
-    }
-
-})
+  const { word, word_type, plural, definition, example, synonyms } = req.body;
+  const wordobj = new Word({
+    word,
+    word_type,
+    plural,
+    definition,
+    example,
+    synonyms,
+  });
+  
+  try {
+    Word.findOne({ word: word }, async (err, worddoc) => {
+      if (worddoc) {
+        return res.json({ message: "Word already exists" });
+      }
+      const newWord = await wordobj.save();
+      res.status(201).json(newWord);
+    });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 //updating one
 router.put("/:id", getWord, async (req, res) => {
-    if (req.body.word != null) {
-        res.word.word = req.body.word
-    }
-    if (req.body.approved != null) {
-        res.word.approved = req.body.approved
-    }
-    if (req.body.word_type != null) {
-        res.word.word_type = req.body.word_type
-    }
-    if (req.body.plural != null) {
-        res.word.plural = req.body.plural
-    }
-    if (req.body.definition != null) {
-        res.word.definition = req.body.definition
-    }
-    if (req.body.example != null) {
-        res.word.example = req.body.example
-    }
-    if (req.body.synonyms != null) {
-        res.word.synonyms = req.body.synonyms
-    }
-    try {
-        const updateWord = await res.word.save()
-        res.json(updateWord)
-    } catch (err) {
-        res.status(400).json({ message: err.message })
-    }
-
-})
+  if (req.body.word != null) {
+    res.word.word = req.body.word;
+  }
+  if (req.body.approved != null) {
+    res.word.approved = req.body.approved;
+  }
+  if (req.body.word_type != null) {
+    res.word.word_type = req.body.word_type;
+  }
+  if (req.body.plural != null) {
+    res.word.plural = req.body.plural;
+  }
+  if (req.body.definition != null) {
+    res.word.definition = req.body.definition;
+  }
+  if (req.body.example != null) {
+    res.word.example = req.body.example;
+  }
+  if (req.body.synonyms != null) {
+    res.word.synonyms = req.body.synonyms;
+  }
+  try {
+    const updateWord = await res.word.save();
+    res.json(updateWord);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 //adding a new meaning
 // router.put("/newmeaning/:id", async (req, res) => {
 //     try {
@@ -82,7 +81,6 @@ router.put("/:id", getWord, async (req, res) => {
 //             "definition": "weak beer",
 //             "example": "a pickle of heavy"
 //         }
-
 
 //         Word.findOneAndUpdate(id, { $push: { "meaning": meanWord } }, { upsert: true, new: true }, (err, doc) => {
 //             if (err) throw err;
@@ -97,50 +95,48 @@ router.put("/:id", getWord, async (req, res) => {
 // })
 //deleting one
 router.delete("/:id", getWord, async (req, res) => {
-    try {
-        await res.word.remove()
-        res.json({ message: "Deleted Word" })
-    } catch (err) {
-        res.status(500).json({ message: err.message })
-    }
-})
+  try {
+    await res.word.remove();
+    res.json({ message: "Deleted Word" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 async function getWord(req, res, next) {
-    let word
-    try {
-        word = await Word.findById(req.params.id)
-        if (word == null) {
-            return res.status(404).json({ message: "Cannot find word" })
-        }
-
-    } catch (err) {
-        return res.status(500).json({ message: err.message })
+  let word;
+  try {
+    word = await Word.findById(req.params.id);
+    if (word == null) {
+      return res.status(404).json({ message: "Cannot find word" });
     }
-    res.word = word
-    next()
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.word = word;
+  next();
 }
 
 async function getQueryWord(req, res, next) {
-    let toSearch = req.params.word
-    let word
-    try {
+  let toSearch = req.params.word;
+  let word;
+  try {
     // word = await Word.find({$text: {$search: toSearch}})// searches only in the word field, doesnt search in the synonyms field
     //word = await Word.find({ "synonyms" : { $regex: `${toSearch}`, $options: 'i' } })// seaarches well in synonyms
     word = await Word.find({
-        "$or": [
-            {$text: {$search: toSearch}},
-            { "synonyms" : { $regex: `${toSearch}`, $options: 'i' } }
-        ]
-    })
+      $or: [
+        { $text: { $search: toSearch } },
+        { synonyms: { $regex: `${toSearch}`, $options: "i" } },
+      ],
+    });
     if (word == null) {
-            return res.status(404).json({ message: "Cannot find word" })
-        }
-
-    } catch (err) {
-        return res.status(500).json({ message: err.message })
+      return res.status(404).json({ message: "Cannot find word" });
     }
-    res.word = word
-    next()
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.word = word;
+  next();
 }
-module.exports = router
+module.exports = router;
 //There needs upsert handling for more than one meaning and more than one word type.
